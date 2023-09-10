6476992c8fb4144277ade51b9991e27c962de22e
HungNguyen <nguyen.phathung123@gmail.com>
2023-09-10 13:53:03 +0700

    gut

---
+++ QLMB/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using QLMB.Models;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.UI;
+
+
+namespace QLMB.Controllers
+{
+    public class HomeController : Controller
+    {
+        database db = new database();
+
+        public ActionResult Index()
+        {
+            var suKienList = db.SuKienUuDais.OrderByDescending(s => s.NgayBatDau);
+            
+            return View(suKienList);
+        }
+
+
+    }
+}
---
+++ QLMB/Controllers/Manager/HumanResourceController.cs
@@ -0,0 +1,42 @@
+﻿using QLMB.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLMB.Controllers.Manager
+{
+    public class HumanResourceController : Controller
+    {
+        database db = new database();
+        // GET: HumanResource
+        public ActionResult HumanResourceMain(string NameSearch)
+        {
+            Session["Page"] = "NS";
+            var data = db.NhanViens.ToList();
+            if (NameSearch != null)
+            {
+                data = data.Where(s => s.MaNV.ToString().Contains(NameSearch) ||
+                                       s.ChucVu.TenCV.ToUpper().Contains(NameSearch) ||
+                                       s.CMND.Trim().Contains(NameSearch) ||
+                                       s.ThongTinND.HoTen.ToUpper().Contains(NameSearch.ToUpper()) ||
+                                       s.TinhTrang.TenTT.ToUpper().Contains(NameSearch.ToUpper())).ToList();
+            }
+            return View(data);
+        }
+
+        public ActionResult Detail(string CMND)
+        {
+            if (Session["Page"] == null)
+            {
+                return RedirectToAction("HumanResourceMain");
+            }
+            else
+            {
+                return View(db.NhanViens.Where(s => s.CMND == CMND).FirstOrDefault());
+            }
+        }
+    }
+}
